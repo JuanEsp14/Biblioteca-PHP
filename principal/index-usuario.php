c3d1f8838914713776b9b198512468575a1a67f9
root <juanmesp@hotmail.com>
2018-05-16 21:37:38 -0300

    Agrego las consultas para ver las cantidades de los libros

--- principal/index-usuario.php
+++ principal/index-usuario.php
@@ -30,7 +30,19 @@
         </tr>
       </thead>
       <tbody>
-        <?php while($row = mysqli_fetch_array($result)){ ?>
+        <?php while($row = mysqli_fetch_array($result)){
+          $aux1 = mysqli_query($link,
+          'SELECT COUNT(*) AS res
+           FROM operaciones
+           WHERE ultimo_estado = "RESERVADO" AND libros_id ='.$row["id"]
+          );
+          $aux2 = mysqli_query($link,
+          'SELECT COUNT(*) AS res
+           FROM operaciones
+           WHERE ultimo_estado = "PRESTADO" AND libros_id ='.$row["id"]
+          );
+          $cantRes = mysqli_fetch_array($aux1);
+          $cantPres = mysqli_fetch_array($aux2)?>
           <tr>
             <td>
               <?php
@@ -39,9 +51,9 @@
             </td>
             <td><a href="show_libro.php?id=<?php echo $row["id"] ?>"><?php echo $row["titulo"] ?></a></td>
             <td><a href="show_autor.php?id=<?php echo $row["autores_id"] ?>"><?php echo $row["nombre"]  ?> <?php echo $row["apellido"] ?></a></td>
-            <td><?php echo $row["cantidad"] ?></td>
+            <td><?php echo $row["cantidad"]?> ( <?php echo $cantRes["res"]?> reservados <?php echo $cantPres["res"]?> prestados)</td>
             <td>
-              <?php if($row["cantidad"] > 0){ ?>
+              <?php if($row["cantidad"] > $cantRes["res"]+$cantPres["res"] ){ ?>
                 <button type="button" class="btn btn-info">Reservar</button>
                 <?php } ?>
             </td>
