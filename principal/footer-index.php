9988091e1546bd702c861b6f7f515f9fb10270a7
EmirFernandes <emir.fernandes6@gmail.com>
2018-06-20 14:59:06 -0300

    Arreglar paginación

--- principal/footer-index.php
+++ principal/footer-index.php
@@ -1,16 +1,20 @@
 <div class="text-center">
   <?php
-    $pag_anterior = $pagina_actual--;
-    $pag_siguiente = $pagina_actual++;
+    $pag_anterior = $pagina_actual - 1 ;
+    $pag_siguiente = $pagina_actual + 1;
     if($pagina_actual == 1){
-      $pag_anterior = $pagina_actual;
+      echo "<a class='btn btn-info disabled' href='#'>Primer p&aacutegina</a> ";
+      echo "<a class='btn btn-info disabled' href='#'>P&aacutegina anterior</a> ";
+    }else{
+      echo "<a class='btn btn-info' href='index.php?pagina=1'>Primer p&aacutegina</a> ";
+      echo "<a class='btn btn-info' href='index.php?pagina=".$pag_anterior."'>P&aacutegina anterior</a> ";
     }
     if($pagina_actual == $total_paginas){
-      $pag_siguiente = $pagina_actual;
+      echo "<a class='btn btn-info disabled' href='#'>P&aacutegina siguiente</a> ";
+      echo "<a class='btn btn-info disabled' href='#'>&Uacuteltima p&aacutegina</a> ";
+    }else{
+      echo "<a class='btn btn-info' href='index.php?pagina=".$pag_siguiente."'>P&aacutegina siguiente</a> ";
+      echo "<a class='btn btn-info' href='index.php?pagina=".$total_paginas."'>&Uacuteltima p&aacutegina</a> ";
     }
-    echo "<a class='btn btn-info' href='index.php?pagina=1'>Primer p&aacutegina</a> ";
-    echo "<a class='btn btn-info' href='index.php?pagina=".$pag_anterior."'>P&aacutegina anterior</a> ";
-    echo "<a class='btn btn-info' href='index.php?pagina=".$pag_siguiente."'>P&aacutegina siguiente</a> ";
-    echo "<a class='btn btn-info' href='index.php?pagina=".$total_paginas--."'>&Uacuteltima p&aacutegina</a> ";
   ?>
 </div>
