229315c42812deda02ed716c9db2792727c7ca0e
EmirFernandes <emir.fernandes6@gmail.com>
2018-06-24 11:58:37 -0300

    arreglar paginacion cuando la busqueda te devuelve vacio

--- principal/footer-index.php
+++ principal/footer-index.php
@@ -2,14 +2,14 @@
   <?php
     $pag_anterior = $pagina_actual - 1 ;
     $pag_siguiente = $pagina_actual + 1;
-    if($pagina_actual == 1){
+    if($pagina_actual <= 1){
       echo "<a class='btn btn-info disabled' href='#'>Primer p&aacutegina</a> ";
       echo "<a class='btn btn-info disabled' href='#'>P&aacutegina anterior</a> ";
     }else{
       echo "<a class='btn btn-info' href='".$url."&pagina=1'>Primer p&aacutegina</a> ";
       echo "<a class='btn btn-info' href='".$url."&pagina=".$pag_anterior."'>P&aacutegina anterior</a> ";
     }
-    if($pagina_actual == $total_paginas){
+    if($pagina_actual >= $total_paginas){
       echo "<a class='btn btn-info disabled' href='#'>P&aacutegina siguiente</a> ";
       echo "<a class='btn btn-info disabled' href='#'>&Uacuteltima p&aacutegina</a> ";
     }else{
